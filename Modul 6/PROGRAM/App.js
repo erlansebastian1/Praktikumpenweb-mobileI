d81d6dc2941c9f324c93fde4623abedfac602add
erlansebastian1 <81007084+erlansebastian1@users.noreply.github.com>
2021-05-27 13:38:04 +0700

    Modul 6

---
+++ Modul 6/PROGRAM/App.js
@@ -0,0 +1,66 @@
+import React, {Component} from 'react';
+import {
+  Container,
+  Header,
+  Content,
+  Left,
+  Body,
+  Icon,
+  Text,
+  ListItem,
+  Thumbnail,
+  Input,
+  Item,
+
+} from 'native-base';
+
+
+let helperArray=require('./userList.json');
+export default class app extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      allUsers: helperArray,
+      usersFiltered: helperArray,
+    };
+  }
+
+  searchUser(textToSearch){
+    this.setState({
+      usersFiltered: this.state.allUsers.filter(i =>
+        i.name.toLowerCase().includes(textToSearch.toLowerCase()),
+        ),
+    });
+  }
+
+  render(){
+    return (
+      <Container>
+        <Header searchBar rounded>
+          <Item>
+            <Icon name ='search' />
+            <Input placeholder ='Search user' onChangeText={text=>{
+              this.searchUser(text)}}/>
+          </Item>
+        </Header>
+        <Content>
+          {this.state.usersFiltered.map((item, index)=>(
+             <ListItem avatar>
+             <Left>
+               <Thumbnail source={{uri: item.image}}/>
+             </Left>
+               <Body>
+                 <Text>{item.name}</Text>
+                 <Text note>{item.address}</Text>
+               </Body>
+           </ListItem>
+
+          ))}
+         
+         
+        </Content>
+      
+      </Container>  
+    );
+  }
+}
